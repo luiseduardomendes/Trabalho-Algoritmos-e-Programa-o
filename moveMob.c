b89a6c0c5e281903ae80292301a9f99447590800
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-27 20:12:28 -0300

    fix parameters of function movement npc

--- moveMob.c
+++ moveMob.c
@@ -8,16 +8,16 @@ typedef struct position{
 }typePos;
 
 // Procedure to control the movement of the enemies
-void npcMovement(struct position npcPos, struct position playerPos, bool silentMove, int rangeViewMob) {
+void npcMovement(typePos *npcPos, typePos playerPos, int rangeViewMob) {
     int flagMov;
-    struct position mobPos;
+    typePos mobPos = *npcPos;
 
     srand(time(NULL));
-    
+
     if (fabs(playerPos.x - mobPos.x) < rangeViewMob && 
                 fabs(playerPos.y - mobPos.y) < rangeViewMob){
         if (playerPos.x > mobPos.x){
-            mobPos.x ++;    
+            mobPos.x ++;
         }
         else {
             mobPos.x --;        
@@ -46,4 +46,16 @@ void npcMovement(struct position npcPos, struct position playerPos, bool silentM
                 break;
         }
     }
+    npcPos->x = mobPos.x;
+    npcPos->y = mobPos.y;
 }
+
+/*  code to test if movement is working
+    typePos playerPos, npcPos;
+    playerPos.x = 10;
+    playerPos.y = 10;
+    npcPos.x = 13;
+    npcPos.y = 13;
+    npcMovement(&npcPos, playerPos, 5);
+    printf("Posição do NPC: %d, %d\n", npcPos.x, npcPos.y);
+    printf("Posição do Player: %d, %d\n", playerPos.x, playerPos.y);*/
