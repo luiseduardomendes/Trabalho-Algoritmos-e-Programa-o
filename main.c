c1acd04c5b8f98add489c22e75bdcbab0ca08404
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-09-27 16:56:28 -0300

    update

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "moveMob.c"
 
 int main() {
     printf("Hello world!");
--- moveMob.c
+++ moveMob.c
@@ -8,79 +8,42 @@ struct position{
 };
 
 // Procedure to control the movement of the enemies
-void npcMovement(struct position npcPos, struct position playerPos, bool silentMove) {
-    int flagMov, rangeViewMob, rangeViewMobLower;
+void npcMovement(struct position npcPos, struct position playerPos, bool silentMove, int rangeViewMob) {
+    int flagMov;
     struct position mobPos;
 
     srand(time(NULL));
-
     
-    if (silentMove){
-        if (fabs(playerPos.x - mobPos.x) < rangeViewMobLower && 
-                    fabs(playerPos.y - mobPos.y) < rangeViewMobLower){
-            if (playerPos.x > mobPos.x){
-                mobPos.x ++;    
-            }
-            else {
-                mobPos.x --;        
-            }
-            if (playerPos.y > mobPos.y){
-                mobPos.y ++;        
-            }    
-            else {
-                mobPos.y --;        
-            }
+    if (fabs(playerPos.x - mobPos.x) < rangeViewMob && 
+                fabs(playerPos.y - mobPos.y) < rangeViewMob){
+        if (playerPos.x > mobPos.x){
+            mobPos.x ++;    
         }
-        else{
-            flagMov = 1 + ((float) rand() / RAND_MAX) * 3;
-            switch (flagMov) {
-                case 1:
-                    mobPos.x ++;            
-                    break;
-                case 2:
-                    mobPos.y ++;            
-                    break;
-                case 3:
-                    mobPos.x --;            
-                    break;
-                case 4:
-                    mobPos.y --;            
-                    break;
-            }
+        else {
+            mobPos.x --;        
         }
-    }  
-    else {
-        if (fabs(playerPos.x - mobPos.x) < rangeViewMob && 
-                    fabs(playerPos.y - mobPos.y) < rangeViewMob){
-            if (playerPos.x > mobPos.x){
-                mobPos.x ++;    
-            }
-            else {
-                mobPos.x --;        
-            }
-            if (playerPos.y > mobPos.y){
-                mobPos.y ++;        
-            }    
-            else {
-                mobPos.y --;        
-            }
+        if (playerPos.y > mobPos.y){
+            mobPos.y ++;        
+        }    
+        else {
+            mobPos.y --;        
         }
-        else{
-            flagMov = 1 + ((float) rand() / RAND_MAX) * 3;
-            switch (flagMov) {
-                case 1:
-                    mobPos.x ++;            
-                    break;
-                case 2:
-                    mobPos.y ++;            
-                    break;
-                case 3:
-                    mobPos.x --;            
-                    break;
-                case 4:
-                    mobPos.y --;            
-                    break;
-            }
+    }
+    else{
+        flagMov = 1 + ((float) rand() / RAND_MAX) * 3;
+        switch (flagMov) {
+            case 1:
+                mobPos.x ++;            
+                break;
+            case 2:
+                mobPos.y ++;            
+                break;
+            case 3:
+                mobPos.x --;            
+                break;
+            case 4:
+                mobPos.y --;            
+                break;
         }
     }
 }
