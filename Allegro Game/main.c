f32bd9aec5ff0f7179fdc354a74dff46c3b1aa4a
Luis Eduardo Pereira Mendes <dudupemendes13@gmail.com>
2021-10-25 14:23:51 -0300

    update drawing

--- Allegro Game/main.c
+++ Allegro Game/main.c
@@ -239,7 +239,6 @@ int main() {
         timeCurrent = clock();
 
 
-
         for(i = 0; i < SIZEMAP_Y; i++){
             for(j = 0; j < SIZEMAP_X; j++){
 
@@ -251,40 +250,21 @@ int main() {
                 }
             }
         }
-        for(i = 0; i < SIZEMAP_Y; i++){
-            for(j = 0; j < SIZEMAP_X; j++){
-                mobFound = 0;
-                playerFound = 0;
-                shurikenFound = 0;
-                for(k = 0; k < NUM_MOBS; k++){
-                    if((npcPos[k].x == j) && (npcPos[k].y == i)){
-                        al_draw_filled_rectangle(npcPos[k].x*MAPSCALE, npcPos[k].y*MAPSCALE, (npcPos[k].x*MAPSCALE)+MAPSCALE, (npcPos[k].y*MAPSCALE)+MAPSCALE,al_map_rgb(200,0,0));
-                        mobFound = 1;
-                    }
-                }
-                if(mobFound == 0){
-                    if (playerPos.shuriken.x == j && playerPos.shuriken.y == i && playerPos.shuriken.throwing){
-                        al_draw_bitmap(shurikenDraw, playerPos.shuriken.x*MAPSCALE, playerPos.shuriken.y*MAPSCALE, 0);
-                        //al_draw_filled_rectangle(playerPos.shuriken.x*MAPSCALE, playerPos.shuriken.y*MAPSCALE, (playerPos.shuriken.x*MAPSCALE)+MAPSCALE, (playerPos.shuriken.y*MAPSCALE)+MAPSCALE,al_map_rgb(0,0,200));
-                    }
-                    else{
-                        for(k = 0; k < NUM_MOBS; k++){
-                            if((npcPos[k].shuriken.x == j) && (npcPos[k].shuriken.y == i) && npcPos[k].shuriken.throwing == 1){
-                                al_draw_bitmap(shurikenDraw, npcPos[k].shuriken.x*MAPSCALE,npcPos[k].shuriken.y*MAPSCALE, 0);
-                                //al_draw_filled_rectangle(npcPos[k].shuriken.x*MAPSCALE,npcPos[k].shuriken.y*MAPSCALE, (npcPos[k].shuriken.x*MAPSCALE)+MAPSCALE, (npcPos[k].shuriken.y*MAPSCALE)+MAPSCALE,al_map_rgb(150,150,150));
-                                shurikenFound = 1;
-                            }
-                        }
-                    }
-                }
-            }
-        }
+
+        for (k = 0; k < NUM_MOBS; k ++)
+            al_draw_filled_rectangle(npcPos[k].x*MAPSCALE, npcPos[k].y*MAPSCALE,(npcPos[k].x*MAPSCALE)+MAPSCALE, (npcPos[k].y*MAPSCALE)+MAPSCALE,al_map_rgb(200,0,0));
+
+        if (playerPos.shuriken.x == j && playerPos.shuriken.y == i && playerPos.shuriken.throwing)
+            al_draw_bitmap(shurikenDraw, playerPos.shuriken.x*MAPSCALE, playerPos.shuriken.y*MAPSCALE, 0);
+
+
+        for(k = 0; k < NUM_MOBS; k++)
+            if((npcPos[k].shuriken.x == j) && (npcPos[k].shuriken.y == i) && npcPos[k].shuriken.throwing == 1)
+                al_draw_bitmap(shurikenDraw, npcPos[k].shuriken.x*MAPSCALE,npcPos[k].shuriken.y*MAPSCALE, 0);
 
         // desenha jogador
-        //al_draw_filled_rounded_rectangle((playerPos.x*MAPSCALE), (playerPos.y*MAPSCALE), (playerPos.x*MAPSCALE)+MAPSCALE, (playerPos.y*MAPSCALE)+MAPSCALE, MAPSCALE/3, MAPSCALE/3, al_map_rgb(255,255,0));
         al_draw_bitmap(naruto,(playerPos.x*MAPSCALE), (playerPos.y*MAPSCALE), 0);
-        //al_draw_textf(font48, al_map_rgb(255,255,255), width/2, height/2+28, 1, "Quadros: %d   Segundos: %d", contador++, contador/60);
-        //al_draw_textf(font48, al_map_rgb(255,255,255), width/2, height/2-28, 1, "Tempo atual: %d    Tempo shuriken: %d", timeCurrent / CLOCKS_PER_SEC, timeBeginShuriken/ CLOCKS_PER_SEC);
+
 
         if ((double)(timeCurrent - timeBeginMovement) / CLOCKS_PER_SEC > 0.5){
             npcMovement(npcPos, playerPos, 5, mapMatrix);
@@ -301,10 +281,6 @@ int main() {
 
         if ((double)(timeCurrent - timeBeginShuriken) / CLOCKS_PER_SEC > 5){
             for(i = 0; i < NUM_MOBS; i ++){
-                /*printf("NPC %d\n", i);
-                //printf("x: %d\ty: %d\tdireção: %d\n", npcPos[i].x,npcPos[i].y,npcPos[i].direction);
-                //printf("x: %d\ty: %d\tdireção: x: %d\ty: %d\n", npcPos[i].shuriken.x, npcPos[i].shuriken.y, npcPos[i].shuriken.movex, npcPos[i].shuriken.movey);
-                */
                 if (!npcPos[i].shuriken.throwing) {
                     npcPos[i].shuriken.x = npcPos[i].x;
                     npcPos[i].shuriken.y = npcPos[i].y;
@@ -417,10 +393,10 @@ int main() {
             //printf("Botao pressionado: %d\n", ev.joystick.button);
             switch (ev.joystick.button){
                 case 0:
-                    printf("botao A\n");
+                    //printf("botao A\n");
                     break;
                 case 1:
-                    printf("botao X\n");
+                    //printf("botao X\n");
                     playerPos.shuriken.throwing = true;
                     playerPos.shuriken.x = playerPos.x;
                     playerPos.shuriken.y = playerPos.y;
@@ -445,30 +421,30 @@ int main() {
                     timeThrowShurikenPlayer = clock();
                     break;
                 case 2:
-                    printf("botao B\n");
+                    //printf("botao B\n");
                     break;
                 case 3:
-                    printf("botao Y\n");
+                    //printf("botao Y\n");
                     break;
                 case 4:
-                    printf("botao LB\n");
+                    //printf("botao LB\n");
                     break;
                 case 5:
                     printf("botao RB\n");
                     break;
                 case 6:
-                    printf("botao options\n");
+                    //printf("botao options\n");
                     openMenu = true;
                     showMenu(width, height, &endOfGame, &openMenu, display, events_queue, joy, joyState, npcPos, playerPos, mapUsed);
                     break;
                 case 7:
-                    printf("botao start\n");
+                    //printf("botao start\n");
                     break;
                 case 8:
-                    printf("botao L3\n");
+                    //printf("botao L3\n");
                     break;
                 case 9:
-                    printf("botao R3\n");
+                    //printf("botao R3\n");
                     break;
             }
         }
