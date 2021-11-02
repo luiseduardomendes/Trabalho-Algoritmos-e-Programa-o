b613f1cfc58c6634a75154ea28ec10ec875288e4
BoslonDeHiggs <eduardobolson2@gmail.com>
2021-11-02 13:52:22 -0300

    Update

--- Allegro Game/attacks.c
+++ Allegro Game/attacks.c
@@ -15,39 +15,42 @@ void updateShurikenPos(typeShur *shuriken, typePos enemy, char mapMatrix[SIZEMAP
 
 void shurikenDir(typePos *npc, typePos playerPos)
 {
-    int i;
-
-
     if (!npc->shuriken.throwing){
         npc->shuriken.x = npc->x;
         npc->shuriken.y = npc->y;
         if(playerPos.y == npc->y)
         {
-            if(playerPos.x > npc->x)
+            if(abs(playerPos.x - npc->x) <= 7)
             {
-                npc->shuriken.movex = 1;//Move right
-                npc->shuriken.movey = 0;
+                if(playerPos.x > npc->x)
+                {
+                    npc->shuriken.movex = 1;//Move right
+                    npc->shuriken.movey = 0;
+                }
+                else
+                {
+                    npc->shuriken.movex = -1;//Move left
+                    npc->shuriken.movey = 0;
+                }
+                npc->shuriken.throwing = true;
             }
-            else
-            {
-                npc->shuriken.movex = -1;//Move left
-                npc->shuriken.movey = 0;
-            }
-            npc->shuriken.throwing = true;
         }
         else if(playerPos.x == npc->x)
         {
-            if(playerPos.y > npc->y)
+            if(abs(playerPos.y - npc->y) <= 7)
             {
-                npc->shuriken.movex = 0;//Move down
-                npc->shuriken.movey = 1;
+                if(playerPos.y > npc->y)
+                {
+                    npc->shuriken.movex = 0;//Move down
+                    npc->shuriken.movey = 1;
+                }
+                else
+                {
+                    npc->shuriken.movex = 0;//Move up
+                    npc->shuriken.movey = -1;
+                }
+                npc->shuriken.throwing = true;
             }
-            else
-            {
-                npc->shuriken.movex = 0;//Move up
-                npc->shuriken.movey = -1;
-            }
-            npc->shuriken.throwing = true;
         }
     }
 }
