1304b81cd97072b3f7a830a3940ed0e68d4f04db
ViniciusBAlves <vinib.alves@hotmail.com>
2021-11-22 00:01:45 -0300

    mais uma contribuição de vinicius boff

--- Allegro Game/src/attacks.c
+++ Allegro Game/src/attacks.c
@@ -101,8 +101,10 @@ void shurikenDirBoss(t_boss *boss, t_player playerPos, ALLEGRO_SAMPLE* throwShur
         }
     }
     if(!throwingBoss) {
-        boss->shurikens[i].x = boss->x;
-        boss->shurikens[i].y = boss->y;
+        for (i = 0; i < 8; i ++){
+            boss->shurikens[i].x = boss->x;
+            boss->shurikens[i].y = boss->y;
+        }
         if(playerPos.y == boss->y || playerPos.x == boss->x)
         {
 
@@ -132,9 +134,9 @@ void shurikenDirBoss(t_boss *boss, t_player playerPos, ALLEGRO_SAMPLE* throwShur
             boss->shurikens[7].movex = -1;//Move up
             boss->shurikens[7].movey = -1;
 
-            for(i = 0; i < 8; i ++)
+            for(i = 0; i < 8; i ++){
                 boss->shurikens[i].throwing = true;
-
+            }
         }
     }
 }
